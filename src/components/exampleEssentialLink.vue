b91bb193ac86b2f36747a98dd7c34d0e3479793a
lucas gomes <lucas.gomes@ibrowse-sds.com.br>
2025-07-02 17:18:31 -0400

    clear all project add congi to eslint

---
+++ src/components/exampleEssentialLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>example</h1>
+</template>
+
+<script>
+export default {
+  name: 'ExampleEssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+  }
+}
+</script>
